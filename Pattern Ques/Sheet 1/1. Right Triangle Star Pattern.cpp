a0cc24b0edf63c5fa555b84a870d1df6258e3aa4
DivyanshuKandpal <67413629+DivyanshuKandpal@users.noreply.github.com>
2022-01-11 18:21:13 +0530

    Add files via upload

---
+++ Pattern Ques/Sheet 1/1. Right Triangle Star Pattern.cpp
@@ -0,0 +1,23 @@
+/* 
+NAME: Divyanshu Kandpal
+Roll No: 1961046
+(GEHU Bhimtal)
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+
+{
+    int i,j, n=5;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j<= i; j++)
+        {
+			cout << "*";
+        }
+        
+	cout <<endl;
+    }
+}
