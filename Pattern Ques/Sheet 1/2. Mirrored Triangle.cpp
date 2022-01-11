366bd63bfa2eef8639c322df06ec16b475ea9cf2
DivyanshuKandpal <67413629+DivyanshuKandpal@users.noreply.github.com>
2022-01-11 20:48:05 +0530

    Add files via upload

---
+++ Pattern Ques/Sheet 1/2. Mirrored Triangle.cpp
@@ -0,0 +1,30 @@
+/* 
+NAME: Divyanshu Kandpal
+Roll No: 1961046
+(GEHU Bhimtal)
+*/
+
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+     int i,j,rows;
+    char ch;
+    cout<<"Enter the number of rows: ";
+    cin>>rows;
+     cout<<"Enter the Symbol for pattern: ";
+    cin>>ch;
+    cout<<"\n";
+    for(i=1; i<=rows; i++){
+	   for(j=0; j<rows-i; j++){
+cout<<" ";
+}
+for(j=0; j<i; j++){
+   cout<<ch;
+}
+ cout<<"\n";
+}
+getch();
+    return 0;
+}
